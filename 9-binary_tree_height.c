487080ef5cacbde2c10a6b5795fbd45ed37d52fb
Pablo-bit7 <122962125+Pablo-bit7@users.noreply.github.com>
2024-06-07 04:03:54 +0200

    Modified 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,18 +8,19 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t height_left, height_right;
+	size_t height_left, height_right;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    /* Recursively get the height of the left and right subtrees */
-    height_left = binary_tree_height(tree->left);
-    height_right = binary_tree_height(tree->right);
+	/* If the node is a leaf, its height is 0 */
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
 
-    /* Return the maximum height of the left and right subtrees */
-    if (height_left > height_right)
-        return (height_left + 1);
-    else
-        return (height_right + 1);
+	/* Recursively get the height of the left and right subtrees */
+	height_left = binary_tree_height(tree->left);
+	height_right = binary_tree_height(tree->right);
+
+	/* Return the maximum height plus 1 for the current edge */
+	return (height_left > height_right ? height_left + 1 : height_right + 1);
 }
